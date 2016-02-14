386a9d150e9931cdf9f0eac7ad43535b656ec6e6
Svendi <Svendi@hrs.fritz.box>
2016-02-14 23:12:51 +0100

    5

--- src/java/models/SchrankController.java
+++ src/java/models/SchrankController.java
@@ -1,51 +1,76 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package models;
 
-import db.Persistence;
+import db.KundePersistence;
+import db.SchrankPersistence;
+import entities.Kunde;
 import entities.Schrank;
 import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.List;
 import javax.ejb.EJB;
+import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 
 /**
  *
  * @author Katha
  */
-
+@Dependent
 public class SchrankController implements Serializable{
     
-    private ArrayList<Schrank> schrankList;
+    private List<Schrank> schrankList;
     
-    @EJB
-    private Persistence persistence;
+    @Inject
+    private KundePersistence kundePersistence;
     
-    private int letzterVergebenerSchrank;
-    private int anzahlSchraenke;
+    @Inject
+    private SchrankPersistence schrankPersistence;
     
     
-    private SchrankController() {
-        schrankList = (ArrayList<Schrank>) persistence.findAlleSchraenke();
-        if(schrankList.isEmpty()){
-            for(int i = 0; i < 50; i++){
-                this.schrankList.add(new Schrank(null));
-            }
-        }
+    private int letzterVergebenerSchrank = -1;
+    private int anzahlSchraenke = 100;
+    
+    
+    SchrankController() {
+        //schrankList = (ArrayList<Schrank>) persistence.findAlleSchraenke();
+
     }
     
+    
+    
+    /**
+     * Initialisiert die Schränke
+     */
     public void initSchrankList(){
-        int schranknummer = 10000;
-        for(int i = 0; i < anzahlSchraenke; i++){
-            schrankList.get(i).setSchranknummer(schranknummer + i);
-        }
+        
+        if(schrankPersistence.findAll().size() == 0){
+            schrankList = new ArrayList();
+            int schranknummer = 10000;
+            for(int i = 0; i < anzahlSchraenke; i++){
+                    this.schrankList.add(new Schrank(i+1,i+1,null));
+                    schrankList.get(i).setSchranknummer(schranknummer + i);
+                    kundePersistence.persistSchrank(this.schrankList.get(i));
+                }
+            }
     }
     
+    /**
+     *
+     * @return Gibt die Nummer eines verfügbaren Schrankes zurück
+     */
     public int schrankVerfuegbar(){
         int verfuegbarerSchrank = -1;
-        for(int i = 0; i < 50; i++){
+        if(schrankList==null){
+            schrankList = schrankPersistence.findAll();
+        }
+        
+       // schrankList = (ArrayList<Schrank>) kundePersistence.findAlleSchraenke();
+        for(int i = 0; i < anzahlSchraenke; i++){
             if(schrankList.get(i).getKunde() == null){
                 int schrankDiff = letzterVergebenerSchrank - schrankList.get(i).getSchranknummer();
                 if(!(schrankDiff == 1 || schrankDiff == -1)){
@@ -59,20 +84,32 @@ public class SchrankController implements Serializable{
         return verfuegbarerSchrank;
     }
     
-    public int einchecken(long id){
+    /**
+     *
+     * @param idKunde
+     * @return
+     */
+    public int einchecken(int kundennummer){
         int leererSchrank = schrankVerfuegbar();
         if(leererSchrank != -1){
-            schrankList.get(leererSchrank).setKunde(persistence.findKundeById(id));
+            if(schrankPersistence.findAll().size() == 0){
+                for(int i =0; i < schrankList.size(); i++){
+                    schrankPersistence.persist(schrankList.get(i));
+                }
+            }
+            schrankList.get(leererSchrank-1).setKunde(kundePersistence.findKundeByKundennummer(kundennummer));
             letzterVergebenerSchrank = leererSchrank;
         }
-        persistence.schrankZuweisen(id, leererSchrank);
+        kundePersistence.schrankZuweisen(kundePersistence.findKundeByKundennummer(kundennummer).getId(), leererSchrank);
         return leererSchrank;
     }
     
-    public static SchrankController getInstance() {
-        return SchrankControllerHolder.INSTANCE;
-    }
     
+    
+    /**
+     *
+     * @param id
+     */
     public void auschecken(long id){
         Schrank auscheckSchrank = findSchrankByKundenId(id);
         if(!(auscheckSchrank == null)){
@@ -80,6 +117,11 @@ public class SchrankController implements Serializable{
         }
     }
     
+    /**
+     *
+     * @param id
+     * @return
+     */
     public Schrank findSchrankByKundenId(long id){
         for(Schrank s : schrankList){
             if(s.getKunde().getId() == id){
@@ -88,10 +130,47 @@ public class SchrankController implements Serializable{
         }
         return null;
     }
-    
-    private static class SchrankControllerHolder {
 
-        private static final SchrankController INSTANCE = new SchrankController();
+    public List<Schrank> getSchrankList() {
+        return schrankList;
     }
+
+    public void setSchrankList(ArrayList<Schrank> schrankList) {
+        this.schrankList = schrankList;
+    }
+
+    public KundePersistence getKundePersistence() {
+        return kundePersistence;
+    }
+
+    public void setKundePersistence(KundePersistence kundePersistence) {
+        this.kundePersistence = kundePersistence;
+    }
+
+    public SchrankPersistence getSchrankPersistence() {
+        return schrankPersistence;
+    }
+
+    public void setSchrankPersistence(SchrankPersistence schrankPersistence) {
+        this.schrankPersistence = schrankPersistence;
+    }
+
+
+    public int getLetzterVergebenerSchrank() {
+        return letzterVergebenerSchrank;
+    }
+
+    public void setLetzterVergebenerSchrank(int letzterVergebenerSchrank) {
+        this.letzterVergebenerSchrank = letzterVergebenerSchrank;
+    }
+
+    public int getAnzahlSchraenke() {
+        return anzahlSchraenke;
+    }
+
+    public void setAnzahlSchraenke(int anzahlSchraenke) {
+        this.anzahlSchraenke = anzahlSchraenke;
+    }
+    
     
 }
